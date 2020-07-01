2981bdf3c5008e0b6a569565c1b38e3889027090
morantaf <fall_moranta@live.fr>
2020-07-01 13:53:57 +0200

    Updating the routes to insert the Profile page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LoginForm from "./components/LoginForm";
 import AppBar from "./components/AppBar";
+import SignupForm from "./components/SignupForm";
 import { Route } from "react-router";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <div>
       <AppBar />
       <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/signup" component={SignupForm} />
+      <Route path="/b/:id" component={ProfilePage} />
     </div>
   );
 }
