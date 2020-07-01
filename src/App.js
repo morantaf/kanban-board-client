0533445d12ffd43deaf53f8660fd0afcb81dcdef
morantaf <fall_moranta@live.fr>
2020-07-01 17:11:35 +0200

    Routing the app to the user profile

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import AppBar from "./components/AppBar";
 import SignupForm from "./components/SignupForm";
 import { Route } from "react-router";
 import ProfilePage from "./components/ProfilePage";
+import { getTokens } from "./manage-tokens";
 
 function App() {
+  const { username } = getTokens();
   return (
     <div>
       <AppBar />
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
-      <Route path="/b/:id" component={ProfilePage} />
+      {username ? <Route path="/b/:username" component={ProfilePage} /> : null}
     </div>
   );
 }
