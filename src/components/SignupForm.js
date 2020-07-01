651cfa062fc1e91bb30a48849e5abbd29be9420f
morantaf <fall_moranta@live.fr>
2020-07-01 10:54:55 +0200

    Copying the LoginForm component and adding the lastName, firstName, username input field

---
+++ src/components/SignupForm.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation, useApolloClient } from "react-apollo";
+import styled from "styled-components";
+import { saveTokens } from "../manage-tokens";
+import { Mutation } from "react-apollo";
+
+//Creation of Styled components
+
+const Form = styled.form`
+  padding: 4em;
+  max-width: 400px;
+  background: white;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  margin-top: 50px;
+  box-shadow: 0px 4px 7px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const TextField = styled.input`
+  padding-top: 10px;
+  padding-right: 2px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  margin-bottom: 15px;
+`;
+
+const Button = styled.button`
+  background: #9933ff;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.7em 1em;
+  border: 2px solid;
+  border-radius: 7px;
+  width: 30%;
+  align-self: center;
+  &:hover {
+    background: white;
+    color: #9933ff;
+  }
+`;
+
+const Title = styled.h2`
+  align-self: center;
+`;
+
+const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      refreshToken
+      accessToken
+    }
+  }
+`;
+
+export default function LoginForm(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+
+  const _confirm = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Mutation
+      mutation={LOGIN_USER}
+      variables={{ email, password }}
+      onCompleted={(data) => _confirm(data)}
+    >
+      {(login, { data }) => (
+        <div>
+          <Form
+            onSubmit={(event) => {
+              event.preventDefault();
+              login({ variable: { email, password } });
+            }}
+          >
+            <Title>Please enter your credentials</Title>
+            <TextField
+              type="text"
+              value={email}
+              name="email"
+              placeholder="E-mail address"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <TextField
+              type="text"
+              value={firstName}
+              name="firstName"
+              placeholder="First name"
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+            <TextField
+              type="text"
+              value={lastName}
+              name="lastName"
+              placeholder="Last Name"
+              onChange={(event) => setLastName(event.target.value)}
+            />
+            <TextField
+              type="text"
+              value={username}
+              name="username"
+              placeholder="Username"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            <TextField
+              type="password"
+              value={password}
+              name="password"
+              placeholder="Enter your password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <Button type="submit">Log in</Button>
+          </Form>
+        </div>
+      )}
+    </Mutation>
+  );
+}
