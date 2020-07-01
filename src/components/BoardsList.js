a14524886afbe1e8a0c1ab3da0b8747eaf7b0260
morantaf <fall_moranta@live.fr>
2020-07-01 17:13:05 +0200

    Creating the query to fetch the data for the boards for the logged in user, Creating the styled components

--- src/components/BoardsList.js
+++ src/components/BoardsList.js
@@ -1,7 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Link } from "react-router-dom";
+const BOARDS_QUERY = gql`
+  {
+    boards {
+      id
+      title
+      description
+    }
+  }
+`;
+
+const Board = styled.a`
+  height: 80px;
+  width: 167px;
+  background-color: cornflowerblue;
+`;
+
+const Boards = styled.div`
+  display: flex;
+`;
 
 function BoardsList() {
-  return <div></div>;
+  return (
+    <div>
+      <Query query={BOARDS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) {
+            console.log(error);
+            return <p>Must authenticate</p>;
+          }
+          return data.boards.map((board) => (
+            <Board href={`/board/${board.id}`}>
+              <h2>{board.title}</h2>
+            </Board>
+          ));
+        }}
+      </Query>
+    </div>
+  );
 }
 
 export default BoardsList;
