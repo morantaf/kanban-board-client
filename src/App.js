21c94901ef8f4a58754494a59fcc1f30acd86d71
morantaf <fall_moranta@live.fr>
2020-07-03 11:42:38 +0200

    Creating the Board file and Routing the component to the correct URL

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { Route } from "react-router";
 import ProfilePage from "./components/ProfilePage";
 import { getTokens } from "./manage-tokens";
 import { Redirect } from "react-router-dom";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div>
       <AppBar />
       <Redirect from="/" to="/b/personal-board" />
+      <Route path="/board/:id" component={Board} />
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
       <Route path={`/b/personal-board`} component={ProfilePage} />
