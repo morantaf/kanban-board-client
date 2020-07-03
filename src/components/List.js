3188095d80336cd6878b68c23a5df86e5207662e
morantaf <fall_moranta@live.fr>
2020-07-03 14:27:39 +0200

    Creating the List component

---
+++ src/components/List.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import { Query, useQuery } from "react-apollo";
+import gql from "graphql-tag";
+
+const Wrapper = styled.div`
+  margin: 10px 4px;
+  width: 272px;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  background-color: #ebecf0;
+  border-radius: 3px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  max-height: 100%;
+  position: relative;
+  white-space: normal;
+`;
+
+const CardWrapper = styled.div`
+  flex: 1 1 auto;
+  margin-bottom: 0;
+  overflow-y: auto;
+  overflow-x: hidden;
+  margin: 0 4px;
+  padding: 0 4px;
+  z-index: 1;
+  min-height: 0;
+`;
+
+const BoldText = styled.p`
+  font-weight: bold;
+`;
+
+const GET_CARDS = gql`
+  query cardsByList($listId: Int!) {
+    cardsByList(listId: $listId) {
+      title
+      description
+      status
+    }
+  }
+`;
+
+function List(props) {
+  const { data, loading, error } = useQuery(GET_CARDS, {
+    variables: { listId: props.id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error : {error.message}</p>;
+
+  const cards = data.cardsByList;
+
+  console.log(cards);
+  return (
+    <Wrapper>
+      <Content>
+        <BoldText>{props.name}</BoldText>
+      </Content>
+    </Wrapper>
+  );
+}
+
+export default List;
