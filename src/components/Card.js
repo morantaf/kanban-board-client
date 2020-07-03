48cc82406f4d52e0c81d4dd2de477b70a7535683
morantaf <fall_moranta@live.fr>
2020-07-03 17:10:52 +0200

    commenting the cursor attribute from the Wrapper component

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Wrapper = styled.div`
   background-color: #fff;
   border-radius: 3px;
   box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
-  cursor: pointer;
+  // cursor: pointer;
   display: block;
   margin-bottom: 8px;
   max-width: 300px;
