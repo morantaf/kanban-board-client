ba5eb736d4e98e71e634ed320179425e78194ec9
morantaf <fall_moranta@live.fr>
2020-07-03 11:36:48 +0200

    Adding the Redirect to the personal-board url

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import SignupForm from "./components/SignupForm";
 import { Route } from "react-router";
 import ProfilePage from "./components/ProfilePage";
 import { getTokens } from "./manage-tokens";
+import { Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <AppBar />
+      <Redirect from="/" to="/b/personal-board" />
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
       <Route path={`/b/personal-board`} component={ProfilePage} />
