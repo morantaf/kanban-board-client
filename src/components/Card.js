47b6d5635deb94a8789ecc87e57c0d5a114c165e
morantaf <fall_moranta@live.fr>
2020-07-03 14:44:31 +0200

    Styling the component

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+  cursor: pointer;
+  display: block;
+  margin-bottom: 8px;
+  max-width: 300px;
+  min-height: 20px;
+  position: relative;
+  text-decoration: none;
+  z-index: 0;
+`;
+
+export default function Card(props) {
+  return <Wrapper>{props.title}</Wrapper>;
+}
