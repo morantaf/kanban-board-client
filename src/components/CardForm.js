514484adcc23a2ea1fb6086472dd91c4f27dd7bb
morantaf <fall_moranta@live.fr>
2020-07-04 16:12:45 +0200

    Adding style components and the icon

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { FaPlus } from "react-icons/fa";
 
 const ADD_CARD = gql`
   mutation addCard($title: String!, $listId: Int!) {
@@ -9,6 +10,37 @@ const ADD_CARD = gql`
       title
     }
   }
+`;
+
+const TitleForm = styled.p`
+  margin-left: 4%;
+  color: #8c8c8c;
+  cursor: pointer;
+`;
+
+const Form = styled.div`
+  margin-left: 4%;
+  margin-bottom: 4%;
+`;
+
+const TextField = styled.input`
+  padding-top: 10px;
+  padding-right: 2px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  margin-bottom: 15px;
+  width: 90%;
+`;
+
+const StyledButton = styled.button`
+  background-color: #5aac44;
+  box-shadow: none;
+  border: none;
+  color: #fff;
+  padding: 6px 10px 6px 10px;
+  border-radius: 5px;
+  margin-left: 2%;
+  margin-bottom: 2%;
 `;
 
 export default function CardForm(props) {
@@ -24,24 +56,29 @@ export default function CardForm(props) {
 
   return (
     <div>
-      <p onClick={() => setShowForm(!showForm)}> Add a card</p>
+      <TitleForm onClick={() => setShowForm(!showForm)}>
+        {" "}
+        <FaPlus /> Add a card
+      </TitleForm>
       {showForm ? (
-        <div>
-          <input
+        <Form>
+          <TextField
             type="text"
             name="title"
             value={title}
             onChange={(event) => setTitle(event.target.value)}
-            placeholder="Add a title to the list"
+            placeholder="Add a title to the card"
           />
           <Mutation
             mutation={ADD_CARD}
             variables={{ title, listId }}
             onCompleted={(data) => _confirm(data)}
           >
-            {(mutation) => <button onClick={mutation}>Add the card</button>}
+            {(mutation) => (
+              <StyledButton onClick={mutation}>Add the card</StyledButton>
+            )}
           </Mutation>
-        </div>
+        </Form>
       ) : null}
     </div>
   );
