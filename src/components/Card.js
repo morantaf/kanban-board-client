73ed03acb27632ccc1b1b5e0d3682ca1cbc2f49b
morantaf <fall_moranta@live.fr>
2020-07-04 16:12:00 +0200

    Adding the delete button and the mutation to delete the Card from the database

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { MdDelete } from "react-icons/md";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
 const Wrapper = styled.div`
   background-color: #fff;
@@ -9,12 +12,49 @@ const Wrapper = styled.div`
   display: block;
   margin-bottom: 8px;
   max-width: 300px;
-  min-height: 20px;
+  min-height: 40px;
   position: relative;
   text-decoration: none;
   z-index: 0;
 `;
 
+const Title = styled.p`
+  padding: 5%;
+`;
+
+const StyledIcon = styled(MdDelete)`
+  position: absolute;
+  right: 1px;
+  bottom: 1px;
+  cursor: pointer;
+  color: #a6a6a6;
+`;
+
+const DELETE_CARD = gql`
+  mutation deleteCard($id: Int!) {
+    deleteCard(id: $id) {
+      deleted
+    }
+  }
+`;
+
 export default function Card(props) {
-  return <Wrapper>{props.title}</Wrapper>;
+  const id = props.id;
+
+  const _deleted = () => {
+    props.refetch();
+  };
+  return (
+    <Wrapper>
+      <Title>{props.title}</Title>
+      <Mutation
+        mutation={DELETE_CARD}
+        variables={{ id }}
+        ignoreResults={true}
+        onCompleted={() => _deleted()}
+      >
+        {(mutation) => <StyledIcon onClick={mutation} />}
+      </Mutation>
+    </Wrapper>
+  );
 }
