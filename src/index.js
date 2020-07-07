e674f5537c7763e0277d385eba62cb0ba0a64f8a
morantaf <fall_moranta@live.fr>
2020-07-07 14:03:32 +0200

    Using the jwt from the local storage instead of access token and refresh token

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   request: (operation) => {
     const tokens = getTokens();
-    if (tokens && tokens.accessToken) {
+    if (tokens && tokens.jwt) {
       operation.setContext({
         headers: {
-          "x-access-token": tokens.accessToken,
-          "x-refresh-token": tokens.refreshToken,
+          authorization: `Bearer ${tokens.jwt}`,
         },
       });
     }
