03f444c77271a742a8d1ae3c44b0c4e3a9f6baf3
morantaf <fall_moranta@live.fr>
2020-07-07 16:59:39 +0200

    Adding the button to pop up the form that'll delete the form

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import gql from "graphql-tag";
-import { Query, useQuery } from "react-apollo";
-import { useParams } from "react-router-dom";
+import { Query, useQuery, Mutation } from "react-apollo";
+import { useParams, Redirect } from "react-router-dom";
 import List from "./List";
 import ListForm from "./ListForm";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import update from "immutability-helper";
+import { FaTimes } from "react-icons/fa";
+import DeleteForm from "./DeleteForm";
 
 const GET_LISTS = gql`
   query ListsByBoard($boardId: Int!) {
@@ -16,6 +18,24 @@ const GET_LISTS = gql`
       id
     }
   }
+`;
+
+const Delete = styled.button`
+  background-color: hsla(0, 0%, 100%, 0.24);
+  display: flex;
+  position: absolute;
+  top: 50px;
+  right: 50px;
+  white-space: normal;
+  color: white;
+  cursor: pointer;
+  border-radius: 3px;
+  margin-top: 10px;
+  height: 34px;
+  width: 150px;
+  align-items: center;
+  place-content: center;
+  border: 0px;
 `;
 
 const GET_BOARD_NAME = gql`
@@ -49,6 +69,7 @@ export default function Board() {
   const params = useParams();
   const boardId = parseInt(params.id);
   const [lists, setLists] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
   const { data, loading, error, refetch } = useQuery(GET_LISTS, {
     variables: { boardId },
@@ -87,6 +108,10 @@ export default function Board() {
             return <Title>{data ? data.board.title : null}</Title>;
           }}
         </Query>
+        <Delete onClick={() => setShowForm(true)}>
+          <FaTimes />
+          <p>Delete board</p>
+        </Delete>
       </Header>
       <DndProvider backend={HTML5Backend}>
         <Div>
@@ -102,6 +127,9 @@ export default function Board() {
           <ListForm refetch={refetch} boardId={boardId} />
         </Div>
       </DndProvider>
+      {showForm ? (
+        <DeleteForm setShowForm={setShowForm} boardId={boardId} />
+      ) : null}
     </Page>
   );
 }
