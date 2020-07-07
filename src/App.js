6607e53e6575e889f75a2fb6a690529efadc8299
morantaf <fall_moranta@live.fr>
2020-07-07 13:58:00 +0200

    Updating the Route to the Profile Page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import AppBar from "./components/AppBar";
 import SignupForm from "./components/SignupForm";
 import { Route } from "react-router";
 import ProfilePage from "./components/ProfilePage";
-import { getTokens } from "./manage-tokens";
-import { Redirect } from "react-router-dom";
+
 import Board from "./components/Board";
 
 function App() {
   return (
     <div>
       <AppBar />
-      {/* <Redirect exact from="/" to="/b/personal-board" /> */}
       <Route path="/board/:id" component={Board} />
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
-      <Route path={`/b/personal-board`} component={ProfilePage} />
+      <Route path="/b/:id" component={ProfilePage} />
     </div>
   );
 }
