43692239b198d9a6f01488a0af1e9cab58cf5a10
morantaf <fall_moranta@live.fr>
2020-09-14 11:31:01 +0200

    Styling the elements

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,24 +1,55 @@
 import React from "react";
 import image from "../img/3594607.jpg";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const StyledImage = styled.img`
   height: 450px;
 `;
 
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 8%;
+  padding: 0 5%;
+`;
+
+const Body = styled.div`
+  margin: 2%;
+  color: #375b65;
+`;
+
+const Title = styled.h1`
+  font-size: 5em;
+  margin: 0;
+`;
+
+const StyledLink = styled(Link)`
+  margin-top: 50px;
+  text-decoration: none;
+  font-size: 1em;
+  font-weight: bold;
+  background: #ff0854;
+  color: #fff;
+  border: none;
+  padding: 15px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
 export default function Homepage() {
   return (
     <div>
-      <div className="flex-container">
-        <div className="flex-item">
-          <h1>Organize your tasks, work faster</h1>
-          <h2>Your Kanban Board for increased productivity</h2>
-          <p></p>
-        </div>
-        <div className="flex-item">
+      <Container>
+        <Body>
+          <Title>Organize your tasks, work faster</Title>
+          <h2>The agile solution to improve your productivity</h2>
+          <StyledLink>Go to your board</StyledLink>
+        </Body>
+        <Body>
           <StyledImage src={image} />
-        </div>
-      </div>
+        </Body>
+      </Container>
     </div>
   );
 }
