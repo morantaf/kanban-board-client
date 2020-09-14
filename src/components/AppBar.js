be8bcd144cf51d4133e4cb816038269bb96b24da
morantaf <fall_moranta@live.fr>
2020-09-14 11:32:41 +0200

    Changing color

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Bar = styled.div`
-  background-color: #730099;
+  background-color: #375b65;
   height: 50px;
   box-shadow: 0px 4px 7px 0px rgba(0, 0, 0, 0.2);
 `;
