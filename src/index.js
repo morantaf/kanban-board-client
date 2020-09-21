cb4bb76d6529a5fdc81aaa8a1a56f75170a036cc
morantaf <fall_moranta@live.fr>
2020-09-21 14:28:37 +0200

    Adding URL variable

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import { getTokens } from "./manage-tokens";
 import "./index.css";
 
+// const BASE_URL = "https://evening-garden-55376.herokuapp.com/graphql";
+const BASE_URL = "http://localhost:4000/graphql";
+
 const client = new ApolloClient({
-  uri: "https://evening-garden-55376.herokuapp.com/graphql",
+  uri: BASE_URL,
   request: (operation) => {
     const tokens = getTokens();
     if (tokens && tokens.jwt) {
